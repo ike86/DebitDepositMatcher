022102c447446ce3b28368f11268fb210b9287d1
Pfeff Ákos György <csopimaci@gmail.com>
2017-01-07 14:55:12 +0100

    Test_DebitDepositMatcher Match ReturnsPairedDebitDepositValues_if_TheyMatch

---
+++ src/DebitDepositMatcher/DebitDepositMatcher.Tests/Test_DebitDepositMatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DebitDepositMatcher.Tests
+{
+    public class Test_DebitDepositMatcher
+    {
+        [TestClass]
+        public class Match
+        {
+            [TestMethod]
+            public void ReturnsPairedDebitDepositValues_if_TheyMatch()
+            {
+                var subject = new DebitDepositMatcher();
+
+                var result = subject.Match(
+                    new[] { 42.0m },
+                    new[] { 42.0m });
+
+                result.Should().BeEquivalentTo(new[]
+                {
+                    new Tuple<decimal,decimal>(42m, 42m)
+                });
+            }
+        }
+    }
+}
---
+++ src/DebitDepositMatcher/DebitDepositMatcher/DebitDepositMatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitDepositMatcher
+{
+    public class DebitDepositMatcher
+    {
+        public IEnumerable<Tuple<decimal, decimal>> Match(IEnumerable<decimal> v1, IEnumerable<decimal> v2)
+        {
+            return new[]
+            {
+                new Tuple<decimal, decimal>(42m, 42m)
+            };
+        }
+    }
+}
