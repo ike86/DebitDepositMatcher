8c52bcbc2c300ba206fc6bbc4055f4adeabdf3c8
Pfeff Ákos György <csopimaci@gmail.com>
2017-01-07 15:48:42 +0100

    DebitDepositMatcher Match ReturnsUnmatchedDebitDepositValues_if_TheyDoNotMatch
    - starting to use AutoFixture

--- src/DebitDepositMatcher/DebitDepositMatcher.Tests/Test_DebitDepositMatcher.cs
+++ src/DebitDepositMatcher/DebitDepositMatcher.Tests/Test_DebitDepositMatcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
 
 namespace DebitDepositMatcher.Tests
 {
@@ -11,14 +12,21 @@ namespace DebitDepositMatcher.Tests
         [TestClass]
         public class Match
         {
+            private IFixture fixture = new Fixture();
+            private DebitDepositMatcher subject;
+
+            public Match()
+            {
+                subject = fixture.Freeze<DebitDepositMatcher>();
+            }
+
             [TestMethod]
             public void ReturnsEmpty_if_PassedEmpty()
             {
-                var subject = new DebitDepositMatcher();
+                var debits = Enumerable.Empty<decimal>();
+                var deposits = debits;
 
-                var result = subject.Match(
-                    Enumerable.Empty<decimal>(),
-                    Enumerable.Empty<decimal>());
+                var result = subject.Match(debits, deposits);
 
                 result.Should().BeEmpty();
             }
@@ -26,16 +34,35 @@ namespace DebitDepositMatcher.Tests
             [TestMethod]
             public void ReturnsPairedDebitDepositValues_if_TheyMatch()
             {
-                var subject = new DebitDepositMatcher();
+                var debits = fixture.CreateMany<decimal>().ToArray();
+                var deposits = debits;
 
-                var result = subject.Match(
-                    new[] { 42.0m },
-                    new[] { 42.0m });
+                var result = subject.Match(debits, deposits);
 
-                result.Should().BeEquivalentTo(new[]
-                {
-                    new Tuple<decimal,decimal>(42m, 42m)
-                });
+                result.Select(t => t.Item1).ShouldBeEquivalentTo(debits,
+                    "because we want all debit items in the result");
+                result.Select(t => t.Item2).ShouldBeEquivalentTo(deposits,
+                    "because we want all deposit items in the result");
+                result.Should().OnlyContain(t => t.Item1 == t.Item2 || t.Item1 == 0 || t.Item2 == 0,
+                    "because a pair should match, or either of its members should be 0");
+                result.Should().HaveCount(debits.Count());
+            }
+
+            [TestMethod]
+            public void ReturnsUnmatchedDebitDepositValues_if_TheyDoNotMatch()
+            {
+                var numberOfItems = Math.Min(fixture.Create<int>(), 5); // for the sake of debugger
+                var debits = fixture.CreateMany<decimal>(numberOfItems).ToArray();
+                var deposits = fixture.CreateMany<decimal>(numberOfItems).ToArray();
+
+                var result = subject.Match(debits, deposits);
+
+                result.Select(t => t.Item1).Where(d => d != 0).ShouldBeEquivalentTo(debits,
+                    "because we want all debit items in the result");
+                result.Select(t => t.Item2).Where(d => d != 0).ShouldBeEquivalentTo(deposits,
+                    "because we want all deposit items in the result");
+                result.Should().OnlyContain(t => t.Item1 == t.Item2 || t.Item1 == 0 || t.Item2 == 0,
+                    "because a pair should match, or either of its members should be 0");
             }
         }
     }
--- src/DebitDepositMatcher/DebitDepositMatcher/DebitDepositMatcher.cs
+++ src/DebitDepositMatcher/DebitDepositMatcher/DebitDepositMatcher.cs
@@ -10,7 +10,18 @@ namespace DebitDepositMatcher
     {
         public IEnumerable<Tuple<decimal, decimal>> Match(IEnumerable<decimal> debits, IEnumerable<decimal> depostis)
         {
-            return debits.Zip(depostis, (debit, deposit) => new Tuple<decimal, decimal>(debit, deposit));
+            foreach (var tuple in debits.Zip(depostis, (debit, deposit) => new Tuple<decimal, decimal>(debit, deposit)))
+            {
+                if (tuple.Item1 == tuple.Item2)
+                {
+                    yield return tuple;
+                }
+                else
+                {
+                    yield return new Tuple<decimal, decimal>(tuple.Item1, 0);
+                    yield return new Tuple<decimal, decimal>(0, tuple.Item2);
+                }
+            }
         }
     }
 }
